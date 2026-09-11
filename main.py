e5b64e330c12f1e65cd9ad586726ff8260261e03
Lydia Yang <lydiayang92@gmail.com>
2026-09-10 17:48:29 -0700

    fetched armor

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fetch.enemies import main as fetch_enemies
+from fetch.armor import main as fetch_armor
 
 def main():
-    fetch_enemies()
+    # fetch_enemies()
+    fetch_armor()
 
 if __name__ == "__main__":
     main()
