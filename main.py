7a725660c239486125fad11c628a6bef4994efde
Lydia Yang <lydiayang92@gmail.com>
2026-09-10 18:05:56 -0700

    fetched weapons

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fetch.enemies import main as fetch_enemies
 from fetch.armor import main as fetch_armor
 from fetch.bows import main as fetch_bow
+from fetch.shields import main as fetch_shields
+from fetch.weapons import main as fetch_weapons
+
 
 def main():
     # fetch_enemies()
     # fetch_armor()
-    fetch_bow()
+    # fetch_bow()
+    # fetch_shields()
+    fetch_weapons()
 
 if __name__ == "__main__":
     main()
