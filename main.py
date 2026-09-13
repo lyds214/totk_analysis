64b9dda860de35728c46691e046c84757353598a
Lydia Yang <lyang485@gatech.edu>
2026-09-12 19:14:07 -0700

    realized that none of the data got fetched

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from fetch.enemies import main as fetch_enemies
-from fetch.armor import main as fetch_armor
-from fetch.bows import main as fetch_bow
-from fetch.shields import main as fetch_shields
-from fetch.weapons import main as fetch_weapons
-from fetch.materials import main as fetch_materials
-from fetch.zonai_devices import main as fetch_zonai_devices
-from fetch.creatures import main as fetch_creatures
-from fetch.boss_fuse_drops import main as fetch_boss_fuse_drops
-from fetch.locations import main as fetch_locations
+
 
 
 
@@ -15,15 +7,8 @@ from fetch.locations import main as fetch_locations
 
 def main():
     # fetch_enemies()
-    # fetch_armor()
-    # fetch_bow()
-    # fetch_shields()
-    # fetch_weapons()
-    # fetch_materials()
-    # fetch_zonai_devices()
-    # fetch_creatures()
-    # fetch_boss_fuse_drops()
-    fetch_locations()
+
+    pass
 
 if __name__ == "__main__":
     main()
